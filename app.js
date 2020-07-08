72e4119adb64cb40415144dd1381d50d7e9bae94
WarrenMfg <warrenmfg@icloud.com>
2020-07-08 10:08:15 -0400

    git init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+const PORT = 3000;
+
+// set view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// middleware
+app.use('/auth', authRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => console.log(`OAuth listening on port ${PORT}`));
